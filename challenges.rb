938c367494b5f4e34a0745729cd06548fd5d8d88
samuel <samiehave9@gmail.com>
2024-07-19 05:26:44 +0300

    Challenge2

--- challenges.rb
+++ challenges.rb
@@ -1,3 +1,5 @@
+# Given a hash of family members, with keys as the title and an array of names as the values, use Ruby's built-in select method to gather only siblings' names into a new array.
+
 family = {  uncles: ["bob", "joe", "steve"],
             sisters: ["jane", "jill", "beth"],
             brothers: ["frank","rob","david"],
@@ -13,5 +15,12 @@ sibling_name = family.select { |k| (k == :sisters) || (k == :brothers) }
 sibling_name = family.select do
     |k, v| (k == :sisters) || (k == :brothers)
 end
+# For one complete array, we use flatten method
+arr = sibling_name.values.flatten
 
-puts sibling_name.values
+p arr
+
+# Look at Ruby's merge method. Notice that it has two versions. What is the difference between merge and merge!? Write a program that uses both and illustrate the differences.
+
+# merge method is non-destructive, it does not change the original hash
+# merge! method is destructive, it changes the original hash
